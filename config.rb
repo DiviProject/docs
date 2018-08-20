877abda3acbb5838d466370e32ad88fba4fbbd1e
99darwin <nicksapsales@gmail.com>
2018-08-20 14:10:36 -0700

    activate relative assets

--- config.rb
+++ config.rb
@@ -43,7 +43,7 @@ configure :build do
   # out the following two lines has been known to help
   activate :minify_css
   activate :minify_javascript
-  # activate :relative_assets
+  activate :relative_assets
   # activate :asset_hash
   # activate :gzip
 end
