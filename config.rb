d79eb5908b074605927dd92b4996b3ce08819112
99darwin <nicksapsales@gmail.com>
2018-08-20 14:12:47 -0700

    relative links, true

--- config.rb
+++ config.rb
@@ -34,8 +34,8 @@ activate :autoprefixer do |config|
 end
 
 # Github pages require relative links
-activate :relative_assets
-set :relative_links, true
+# activate :relative_assets
+# set :relative_links, true
 
 # Build Configuration
 configure :build do
@@ -44,6 +44,7 @@ configure :build do
   activate :minify_css
   activate :minify_javascript
   activate :relative_assets
+  set :relative_links, true
   # activate :asset_hash
   # activate :gzip
 end
